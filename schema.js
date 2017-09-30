3f18031901db2a8809f1f4ff71d134bcfc6077aa
or9400 <or9400@gmail.com>
2017-09-30 17:07:49 +0300

    changes

--- schema.js
+++ schema.js
@@ -1,6 +1,4 @@
 const fetch = require('node-fetch')
-const util = require('util')
-const parseXML = util.promisify(require('xml2js').parseString)
 const {
   GraphQLSchema,
   GraphQLObjectType,
@@ -9,73 +7,33 @@ const {
   GraphQLList
 } = require('graphql')
 
-function translate(lang, str) {
-  // Google Translate API is a paid (but dirt cheap) service. This is my key
-  // and will be disabled by the time the video is out. To generate your own,
-  // go here: https://cloud.google.com/translate/v2/getting_started
-  const apiKey =
-    'AIzaSyBN-bwtos8sKU6X84wkrdjtCF7uzng6kgQ'
-	const url =
-    'https://www.googleapis.com' +
-    '/language/translate/v2' +
-  	'?key=' + apiKey +
-    '&source=en' +
-    '&target=' + lang +
-    '&q=' + encodeURIComponent(str)
-  return fetch(url)
- 		.then(response => response.json())
-	  .then(parsedResponse =>
-    	parsedResponse
-      	.data
-        .translations[0]
-        .translatedText
-    )
-}
+const photosApiUrl = "https://jsonplaceholder.typicode.com/photos"
 
-const BookType = new GraphQLObjectType({
-  name: 'Book',
+const PhotoType = new GraphQLObjectType({
+  name: 'Photo',
   description: '...',
 
   fields: () => ({
     title: {
       type: GraphQLString,
-      args: {
-        lang: { type: GraphQLString }
-      },
-      resolve: (xml, args) => {
-        const title = xml.GoodreadsResponse.book[0].title[0]
-        return args.lang ? translate(args.lang, title) : title
+      resolve: json => {
+        console.log(json)
+        return JSON.parse(json).title
       }
     },
-    isbn: {
-      type: GraphQLString,
-      resolve: xml => xml.GoodreadsResponse.book[0].isbn[0]
-    }
-  })
-})
-
-const AuthorType = new GraphQLObjectType({
-  name: 'Author',
-  description: '...',
-
-  fields: () => ({
-    name: {
-      type: GraphQLString,
-      resolve: xml =>
-        xml.GoodreadsResponse.author[0].name[0]
-    },
-    books: {
-      type: new GraphQLList(BookType),
-      resolve: xml => {
-        const ids = xml.GoodreadsResponse.author[0].books[0].book.map(elem => elem.id[0]._)
-        console.log('fetching bookkks!')
-        return Promise.all(ids.map(id =>
-          fetch(`https://www.goodreads.com/book/show/${id}.xml?key=42tmzmwXfJJzHcbXlBRg`)
-            .then(response => response.text())
-            .then(parseXML)
-        ))
+    albumId:{
+      type: GraphQLInt,
+      resolve: json => {
+        console.log(json)
+        return JSON.parse(json).albumId
       }
-
+    },
+    thumbnailUrl:{
+      type: GraphQLString,
+      resolve: json => {
+        console.log(json)
+        return JSON.parse(json).thumbnailUrl
+      }
     }
   })
 })
@@ -86,16 +44,15 @@ module.exports = new GraphQLSchema({
     description: '...',
 
     fields: () => ({
-      author: {
-        type: AuthorType,
+      photo: {
+        type: PhotoType,
         args: {
           id: { type: GraphQLInt }
         },
         resolve: (root, args) => fetch(
-          `https://www.goodreads.com/author/show.xml?id=${args.id}&key=42tmzmwXfJJzHcbXlBRg `
-        )
-        .then(response => response.text())
-        .then(parseXML)
+          `${photosApiUrl}/${args.id}`
+          
+        ).then(response => response.text())  
       }
     })
   })
--- serve.js
+++ serve.js
@@ -4,10 +4,18 @@ const app = express()
 
 const schema = require('./schema')
 
+var { graphql, buildSchema } = require('graphql');
+
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true
 }))
+
+app.get('/photos/:id', function (req, res) {
+  graphql(schema, `{photo(id:${req.params.id}){title,albumId,thumbnailUrl}}`).then((response) => {
+    res.json(response);
+  });
+});
 
 app.listen(4000)
 console.log('Listening ...')
